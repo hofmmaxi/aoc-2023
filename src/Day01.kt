49965c73d317c6c70e8414b28ce55cb2c40ea199
Maximilian Hofmann <mh@wildemail.de>
2023-12-10 17:10:53 +0100

    finished Day01 pt. 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numRegex = "[0-9]".toRegex()
+
+        var result = 0
+
+        for (line in input) {
+            val nums = numRegex.findAll(line).map { it.value.toInt() }
+            val sum = (buildString {
+                append(nums.first())
+                append(nums.last())
+            }).toInt()
+            result += sum
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +22,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
